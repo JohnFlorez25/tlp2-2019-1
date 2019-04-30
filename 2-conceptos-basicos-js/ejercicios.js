3853d801266ce3aef560316ecfad6d83fc581ab0
John Florez <johnflorez1@Macintoshs-MacBook-Pro.local>
2019-04-30 14:20:42 -0500

    fundamentos javascript

---
+++ 2-conceptos-basicos-js/ejercicios.js
@@ -0,0 +1,183 @@
+/*
+
+            Arrow Functions
+
+---------------------------------------------
+1.- Hacer una función que convierta de grados
+    centígrados a Farenheit
+---------------------------------------------
+*/
+
+
+/*
+---------------------------------------------
+2.- Hacer una función que muestre la tabla de
+    multiplicar de un número
+---------------------------------------------
+*/
+
+
+//tablaDeMultiplicar(5);
+
+/*
+---------------------------------------------
+3.- Función que pida N parametros y devuelva
+    cuantos parámetros se le pasaron
+---------------------------------------------
+*/
+
+/* El objeto "arguments" solo puede ser usado con las funciones
+   clásicas; no puede ser utilizado dentro de arrow functions
+*/
+
+/*
+---------------------------------------------
+4.- Leer un arreglo de enteros y sacar su
+    media y promedio
+---------------------------------------------
+*/
+
+//console.log( obtenerPromedio(enteros) );
+
+/*
+---------------------------------------------
+5.- Leer el nombre y sueldo de 8 empleados y
+    mostrar el nombre y sueldo del empleado
+    que más gana
+---------------------------------------------
+*/
+
+
+/*
+---------------------------------------------
+6.- Guardar en un array los 20 primeros
+    números pares
+---------------------------------------------
+*/
+
+
+/*
+---------------------------------------------
+7.- Hacer una funcion que calcule el tiempo
+    necesario para que un automóvil que se
+    mueve con una velocidad de 73000 m/h
+    recorra una distancia de 120 km.
+              (TIEMPO = d/v)
+---------------------------------------------
+*/
+
+
+/*
+---------------------------------------------
+8.- Crear una funcion que reciba como
+    parametro una oración y defina si
+    es palindromo
+---------------------------------------------
+*/
+
+
+/*
+---------------------------------------------
+9.- Mostrar los primeros 10 dígitos de la
+    serie Fibbonacci
+---------------------------------------------
+*/
+
+/*
+
+            Condicionales
+
+---------------------------------------------
+10.- Escribe una función que halle 
+     el máximo entre dos números 
+---------------------------------------------
+*/
+
+/*
+---------------------------------------------
+11.- Escribe una función que halle 
+     el máximo entre tres números
+---------------------------------------------
+*/
+
+/*
+---------------------------------------------
+12.- Escribe una función  que cheque si un 
+     número es negativo, positivo o cero
+---------------------------------------------
+*/
+
+/*
+---------------------------------------------
+13.- Escribe una función para verificar si 
+     un número es divisible entre 5 y 11 
+     o no lo es
+---------------------------------------------
+*/
+
+/*
+---------------------------------------------
+14.- Escriba una función para ingresar 
+     cualquier letra del alfabeto y verifique 
+     si es vocal o consonante
+---------------------------------------------
+*/
+
+/*
+
+            Ciclos For
+
+---------------------------------------------
+15.- Escribe una función que imprima todos
+     los números naturales de 1 hasta n
+---------------------------------------------
+*/
+
+
+/*
+---------------------------------------------
+16.- Escribe una función que imprima todos
+     los números naturales en reversa, 
+     desde n hasta 1.
+---------------------------------------------
+*/
+
+
+/*
+---------------------------------------------
+17.- Escribe una función que imprima todos
+     los números pares entre 1 y 100
+---------------------------------------------
+*/
+
+
+/*
+---------------------------------------------
+18.- Escribe una función que imprima todos
+     los números nones entre 1 y 100
+---------------------------------------------
+*/
+
+
+/*
+---------------------------------------------
+19.- Escribe una función que halle la suma
+     de todos los numeros naturales entre 
+     1 y n
+---------------------------------------------
+*/
+
+
+/*
+---------------------------------------------
+20.- Escribe una función que imprima una
+     pirámide de astericos como la siguiente:
+       
+                     *
+                    * *
+                   * * *
+                  * * * *
+                 * * * * *
+---------------------------------------------
+*/
+  

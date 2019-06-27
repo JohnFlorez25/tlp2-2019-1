998eb95bf1da7ca84f9773fb46f398008a50b6d7
JohnFloro <john_florez82102@elpoli.edu.co>
2019-06-26 23:28:38 -0500

    update index

--- my-first-server-express/index.js
+++ my-first-server-express/index.js
@@ -12,13 +12,44 @@ y correr el servidor
 */
 
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.get('/', function(req, res, next){
+//configuracion para poder recibir el body
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
     res.send({ Hello : "Poli"});
 })
 
-const server = app.listen(5600, function(){
-    console.log(`Listening http://localhost:${server.address().port}`)
+app.get('/home', (req, res)=>{
+    res.send({message:'Mensaje desde Home'})
+})
+
+//Parametros
+app.get('/user/:uid',(req,res)=>{
+    console.log(req.params);
+    const uid = req.params.uid;
+    res.send({mesagge: `Id buscado: ${uid}`})
+});
+
+app.post('/create/user',(req,res) => {
+    console.log(req.body);
+    const {name,last_name,age,is_active} = req.body;
+    res.status(201).send({
+        id:'19',
+        name,
+        last_name,
+        age,
+        is_active
+    });
+});
+
+const server = app.listen(5600, 
+    function(){
+    console.log(`Listening 
+    http://localhost:
+    ${server.address().port}`)
 })
